4df094ee716b2c935ae44ef8c1f0b97893ed4815
wojtaszczyk <wojtaszczykpatrycja@gmail.com>
2024-08-09 20:55:58 +0200

    zmiany zmiany

--- tests/calculator.py
+++ tests/calculator.py
@@ -1,8 +1,6 @@
 class Calculator:
-
-    def add(self,a,b):
-        return a+b
+    def add(self, a, b):
+        return a + b
 
     def multiply(self, a, b):
         return a * b
-
--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,29 +1,30 @@
-import os
-import unittest
-
-from selenium import webdriver
-
-
-class TestExample(unittest.TestCase):
-
-    def setUp(self):
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        self.driver = webdriver.Remote(
-            command_executor=os.getenv('SELENIUM_REMOTE_URL', 'http://localhost:4444/wd/hub'),
-            options=options
-        )
-
-
-    def test_title(self):
-        driver = self.driver
-        driver.get("http://demo-store.seleniumacademy.com/"),
-        driver.save_screenshot("screenshot/test.png")
-
-    def tearDown(self):
-        self.driver.close()
-
-if __name__ == '__main__':
-    unittest.main()
+# import os
+# import unittest
+#
+# from selenium import webdriver
+#
+#
+# class TestExample(unittest.TestCase):
+#
+#     def setUp(self):
+#         options = webdriver.ChromeOptions()
+#         options.add_argument("--headless")
+#         options.add_argument("--no-sandbox")
+#         options.add_argument("--disable-dev-shm-usage")
+#         self.driver = webdriver.Remote(
+#             command_executor=os.getenv('SELENIUM_REMOTE_URL', 'http://localhost:4444/wd/hub'),
+#             options=options
+#         )
+#
+#
+#     def test_title(self):
+#         driver = self.driver
+#         driver.get("http://demo-store.seleniumacademy.com/")
+#         driver.save_screenshot("screenshot/test.png")
+#
+#     def tearDown(self):
+#         self.driver.close()
+#
+# if __name__ == '__main__':
+#     unittest.main()
+#
